92dcb389a274bc6b8ce357955e3cae85a11818bd
luodong <luodongdongdongdongdong@chaolu.com.cn>
2022-04-26 17:29:28 +0800

    init

---
+++ app/src/main/java/com/luodong/lib_androidx/MainActivity.java
@@ -0,0 +1,14 @@
+package com.luodong.lib_androidx;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
